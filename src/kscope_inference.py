1977c2ff09f0d6e9e0f188f32e56419197291107
CamBish <camb712@gmail.com>
2024-11-28 11:01:26 -0500

    update kaleidoscope_inference to check .env file

--- src/kscope_inference.py
+++ src/kscope_inference.py
@@ -1,14 +1,20 @@
+import os
+
 import openai
+import dotenv
+
 
 from time import sleep
+
+project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+dotenv_path = os.path.join(project_dir, ".env")
+dotenv.load_dotenv(dotenv_path)
 
 client = openai.OpenAI()
 
 messages = [
-    {
-        "role": "system", "content": "You are a helpful assistant. Please answer the following question.",
-        "role": "user", "content": "what is the capital of Canada?"
-    }
+    {"role": "system", "content": "You are a helpful assistant. Please answer the following question."},
+    {"role": "user", "content": "what is the capital of Canada?"}
 ]
 
 output = None
